f3fc55bacb1437b5a831ef2447cab78b5db9f7d9
aga2zak <aga2zak@gmail.com>
2019-04-13 21:34:27 +0200

    added parameterized tests for cross browsers

---
+++ ITCrowdProject/src/main/java/pl/infoshare/tests/PurchaseCrossBrowsersTest.java
@@ -0,0 +1,193 @@
+package pl.infoshare.tests;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.*;
+import org.junit.experimental.categories.Category;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Suite;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import pl.infoshare.categories.*;
+import pl.infoshare.dataModels.Address;
+import pl.infoshare.dataModels.Bag;
+import pl.infoshare.dataModels.RegisteredUser;
+import pl.infoshare.generators.BagGenerator;
+import pl.infoshare.pages.*;
+import pl.infoshare.pages.components.CategoriesMenu;
+
+import java.util.Arrays;
+import java.util.Collection;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@RunWith(Parameterized.class)
+public class PurchaseCrossBrowsersTest {
+
+    private RemoteWebDriver driver;
+    private BasePage basePage;
+    private Address address;
+    private RegisteredUser user;
+    private Bag randomBag;
+    private Bag randomBagNext;
+    private Bag randomLaptopBag;
+
+    public PurchaseCrossBrowsersTest(String browser){
+        if (browser.equals("chrome")){
+            this.driver = new ChromeDriver();
+        } else if (browser.equals("firefox"))
+            this.driver = new FirefoxDriver();
+    }
+
+    @Parameterized.Parameters
+    public static Collection data() {
+        return Arrays.asList(new Object[]{ "chrome", "firefox"});
+    }
+
+    @BeforeClass
+    public static void beforeAll() {
+        WebDriverManager.chromedriver().setup();
+        WebDriverManager.firefoxdriver().setup();
+    }
+
+    @Before
+    public void startBrowser() {
+       this.basePage = new BasePage(driver);
+        this.user = new RegisteredUser(true);
+        this.randomBag = BagGenerator.generateRandomBag();
+        this.randomBagNext = BagGenerator.generateRandomBag();
+        this.randomLaptopBag = BagGenerator.generateCategoryRandomBag("Laptop bags");
+    }
+
+    @After
+    public void closeBrowser() {
+        basePage.close();
+    }
+
+    @Category(PurchaseTests.class)
+    @Test
+    public void purchase() {
+        MainPage mainPage = new MainPage(driver);
+        mainPage.chooseHandbagsCategory();
+
+        HandbagCataloguePage handbagCataloguePage = new HandbagCataloguePage(driver);
+        handbagCataloguePage.addToCart();
+        handbagCataloguePage.clickOnShipping();
+        handbagCataloguePage.checkout();
+
+        ReviewYourOrderPage reviewYourOrderPage = new ReviewYourOrderPage(driver);
+        reviewYourOrderPage.proceedToCheckout();
+
+        CheckoutPage checkoutPage = new CheckoutPage(driver);
+        checkoutPage.sectionFullName(user);
+        checkoutPage.fillInSectionCountryState(user);
+        checkoutPage.submitOrder();
+
+        try {
+            Thread.sleep(5000);
+            OrderConfirmationPage orderConfirmationPage = new OrderConfirmationPage(driver);
+            assertThat(orderConfirmationPage.readOrderCompleted()).isEqualTo("Order completed");
+        } catch (Throwable error) {
+            Assert.fail("Timeout waiting is completed");
+        }
+
+    }
+
+    @Category(AddToCartTest.class)
+    @Test
+    public void addToCart() {
+        MainPage mainPage = new MainPage(driver);
+        mainPage.chooseHandbagsCategory();
+
+        HandbagCataloguePage handbagCataloguePage = new HandbagCataloguePage(driver);
+        handbagCataloguePage.addToCart();
+        handbagCataloguePage.clickOnShipping();
+        handbagCataloguePage.checkout();
+
+        ReviewYourOrderPage reviewYourOrderPage = new ReviewYourOrderPage(driver);
+        assertThat(reviewYourOrderPage.getItem().getText()).isEqualTo("Chic vintage DeVille");
+        assertThat(reviewYourOrderPage.getQuantity().getValue()).isEqualTo("1");
+        assertThat(reviewYourOrderPage.getPrice().getText()).isEqualTo("$78.00");
+        assertThat(reviewYourOrderPage.getTotal().getText()).isEqualTo("$78.00");
+    }
+
+    @Category(AddToCartRandomBagTest.class)
+    @Test
+    public void addToCartRandomBag() {
+        MainPage mainPage = new MainPage(driver);
+        mainPage.chooseCategory(randomBag.getCategory());
+
+        CategoriesMenu categoriesMenu = new CategoriesMenu();
+        CatalouqePage catalouqePage = new CatalouqePage(driver, categoriesMenu.switchToPage(randomBag.getCategory()));
+        catalouqePage.getRandomBag(randomBag.getName());
+        catalouqePage.addToCart();
+        catalouqePage.clickOnShipping();
+        catalouqePage.checkout();
+
+        ReviewYourOrderPage reviewYourOrderPage = new ReviewYourOrderPage(driver);
+        assertThat(reviewYourOrderPage.getItem().getText()).isEqualTo(randomBag.getName());
+        assertThat(reviewYourOrderPage.getQuantity().getValue()).isEqualTo("1");
+        assertThat(Double.parseDouble(reviewYourOrderPage.getPrice().getText().substring(1))).isEqualTo(randomBag.getPrice().doubleValue());
+        assertThat(Double.parseDouble(reviewYourOrderPage.getTotal().getText().substring(1))).isEqualTo(randomBag.getPrice().doubleValue());
+    }
+
+    @Category(AddToCartNextRandomBagTest.class)
+    @Test
+    public void addToCartNextRandomBag() {
+        MainPage mainPage = new MainPage(driver);
+        mainPage.chooseCategory(randomBag.getCategory());
+
+        CategoriesMenu categoriesMenu = new CategoriesMenu();
+
+        CatalouqePage catalouqePage = new CatalouqePage(driver, categoriesMenu.switchToPage(randomBag.getCategory()));
+        catalouqePage.getRandomBag(randomBag.getName());
+        catalouqePage.addToCart();
+        catalouqePage.clickOnShipping();
+
+        MainPage mainPageNext = new MainPage(driver);
+        mainPageNext.chooseCategory(randomBagNext.getCategory());
+        CatalouqePage catalouqePageNext = new CatalouqePage(driver, categoriesMenu.switchToPage(randomBagNext.getCategory()));
+        catalouqePageNext.getRandomBag(randomBagNext.getName());
+        catalouqePageNext.addToCart();
+        catalouqePageNext.clickOnShipping();
+        catalouqePageNext.checkout();
+
+        ReviewYourOrderPage reviewYourOrderPage = new ReviewYourOrderPage(driver);
+        assertThat(Double.parseDouble(reviewYourOrderPage.getTotalsSubtotal().getText().substring(1)))
+                .isEqualTo(randomBag.getPrice().doubleValue() + randomBagNext.getPrice().doubleValue());
+        assertThat(Double.parseDouble(reviewYourOrderPage.getTotalsTotal().getText().substring(1)))
+                .isEqualTo(randomBag.getPrice().doubleValue() + randomBagNext.getPrice().doubleValue());
+    }
+
+    @Category(PurchaseLaptopBagTest.class)
+    @Test
+    public void purchaseLaptopBag() {
+
+        MainPage mainPage = new MainPage(driver);
+        mainPage.chooseCategory(randomLaptopBag.getCategory());
+
+        LaptopBagsCataloguePage laptopBagsCataloguePage = new LaptopBagsCataloguePage(driver);
+        laptopBagsCataloguePage.getRandomBag(randomLaptopBag.getName());
+        laptopBagsCataloguePage.addToCart();
+        laptopBagsCataloguePage.clickOnShipping();
+        laptopBagsCataloguePage.checkout();
+
+        ReviewYourOrderPage reviewYourOrderPage = new ReviewYourOrderPage(driver);
+        reviewYourOrderPage.proceedToCheckout();
+
+        CheckoutPage checkoutPage = new CheckoutPage(driver);
+        checkoutPage.sectionFullName(user);
+        checkoutPage.fillInSectionCountryState(user);
+        checkoutPage.submitOrder();
+
+        try {
+            Thread.sleep(5000);
+            OrderConfirmationPage orderConfirmationPage = new OrderConfirmationPage(driver);
+            assertThat(orderConfirmationPage.readOrderCompleted()).isEqualTo("Order completed");
+        } catch (Throwable error) {
+            Assert.fail("Timeout waiting is completed");
+        }
+    }
+}
